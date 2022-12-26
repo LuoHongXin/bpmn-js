d3495697231cddfce89e005b70a29dc86b56d75b
kamiaiqiaodaima <954108736@qq.com>
2022-12-26 09:51:36 +0800

    调整文件目录结构

--- bpmn-js-example/src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div ref="canvas"></div>
-</template>
-
-<style scoped></style>
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// 引入相关的依赖
+import BpmnModeler from "bpmn-js/lib/Modeler";
+import { ref, onMounted } from "vue";
+const canvas = ref();
+// 初始化
+const init = () => {
+  console.log(canvas.value, "canvas");
+  // 获取到属性ref为“canvas”的dom节点
+  // 建模
+  // const bpmnModeler = ref(
+  //   new BpmnModeler({
+  //     container: canvas,
+  //   })
+  // );
+  // return { bpmnModeler };
+};
+
+onMounted(() => {
+  init();
+});
+</script>
+
+<template>
+  <div ref="canvas"></div>
+</template>
+
+<style scoped></style>
